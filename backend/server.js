2df75dd376b274cb0a6dc8c12484da392a9a33e7
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-05-11 09:39:19 +0100

    Update CORS

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 ========================= */
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://frontend-lv6c.onrender.com",
+  process.env.FRONTEND_URL,
 ];
 
 app.use(

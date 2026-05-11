332fdb570c309d6a567c4cbe58d6328d7d4fdcda
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-05-11 09:52:52 +0100

    Update

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 ========================= */
 const allowedOrigins = [
   "http://localhost:5173",
-  process.env.FRONTEND_URL,
+  "https://flashio-frontend.onrender.com",
 ];
 
 app.use(

49c81e375e7e5ec87fd23a78fdad2cbc7f819496
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-05-14 10:39:03 +0100

    “Update”

--- backend/src/seeders/20260513093407-seed-categories.cjs
+++ backend/src/seeders/20260513093407-seed-categories.cjs
@@ -4,12 +4,12 @@
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('Categories', [
-      { name: 'Restaurants', icon: '🍽️', path: '/restaurants', color: 'bg-orange-700', text: 'text-orange-600', createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Shops',       icon: '🛍️', path: '/shops',       color: 'bg-red-100',    text: 'text-blue-600',   createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Mall',        icon: '🛒', path: '/mall',        color: 'bg-purple-100', text: 'text-purple-600', createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Local Market',icon: '🥬', path: '/market',      color: 'bg-green-100',  text: 'text-yellow-600', createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Pharmacies',  icon: '🏥', path: '/pharmacies',  color: 'bg-blue-100',   text: 'text-green-600',  createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Laundry',     icon: '🧺', path: '/laundry',     color: 'bg-yellow-100', text: 'text-red-600',    createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Restaurants', icon: '🍽️', path: '/restaurants', theme: 'orange', text: 'text-orange-600', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Shops',       icon: '🛍️', path: '/shops',       theme: 'red', text: 'text-blue-600',   createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Mall',        icon: '🛒', path: '/mall',        theme: 'purple', text: 'text-purple-600', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Local Market',icon: '🥬', path: '/market',      theme: 'green', text: 'text-yellow-600', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Pharmacies',  icon: '🏥', path: '/pharmacies',  theme: 'blue', text: 'text-green-600',  createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Laundry',     icon: '🧺', path: '/laundry',     theme: 'yellow', text: 'text-red-600',    createdAt: new Date(), updatedAt: new Date() },
     ]);
   },
   down: async (queryInterface) => {

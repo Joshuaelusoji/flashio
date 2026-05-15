0d9dd20835416bb4d8fa9a89cfacfe6a3f988089
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-05-15 06:57:03 +0100

    Fix restaurants seeder with uuid

--- backend/src/migrations/20260513152307-create-restaurants.cjs
+++ backend/src/migrations/20260513152307-create-restaurants.cjs
@@ -1,21 +1,48 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Restaurants', {
-      id: { type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true },
-      name:      { type: Sequelize.STRING, allowNull: false },
-      path:      { type: Sequelize.STRING, allowNull: false },
-      image:     { type: Sequelize.STRING, allowNull: false },
-      location:  { type: Sequelize.STRING, allowNull: false },
-      createdAt: { type: Sequelize.DATE, allowNull: false },
-      updatedAt: { type: Sequelize.DATE, allowNull: false },
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      location: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.dropTable('Restaurants');
-
-  }
+  },
 };

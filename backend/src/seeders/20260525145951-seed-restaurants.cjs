19e191d9db8ab27566a81f5019bacd6d9d483048
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-05-26 15:20:29 +0100

    Update

--- backend/src/seeders/20260525145951-seed-restaurants.cjs
+++ backend/src/seeders/20260525145951-seed-restaurants.cjs
@@ -24,7 +24,7 @@ module.exports = {
           id: uuidv4(),
           path: "iya-ruka",
           name: "Iya Ruka",
-          image: "/images/restaurants/Iyaruka.jpg",
+          image: "/images/restaurants/Tantalizer.jpeg",
           location: "Obande",
           deliveryTime: "15-25 mins",
           deliveryFee: 400,

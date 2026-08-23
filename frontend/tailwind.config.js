982f4c2b939317c66ed57f5e86c0d6071749ac3b
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-08-23 19:04:20 +0100

    Update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
+
+  safelist: [
+    "bg-red-300",
+    "text-violet-500",
+  ],
+
   theme: {
     extend: {},
   },
+
   plugins: [
-    require('tailwind-scrollbar-hide')
+    require("tailwind-scrollbar-hide")
   ],
 }
